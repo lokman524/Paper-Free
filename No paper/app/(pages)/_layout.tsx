82b0bc3f7ed2dc55ad5532e667734172fad57d29
lokman524 <79215944+lokman524@users.noreply.github.com>
2025-07-06 18:40:15 +0800

    Update _layout.tsx

--- No paper/app/(pages)/_layout.tsx
+++ No paper/app/(pages)/_layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
         {/* Stack navigator with all UI elements hidden */}
         <Stack
           screenOptions={{
-            headerShown: true,          // Hide all headers
+            headerShown: false,          // Hide all headers
             animation: 'none',           // Disable transition animations
             gestureEnabled: false,       // Disable swipe gestures
             contentStyle: {
