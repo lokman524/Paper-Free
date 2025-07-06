f122463e5c3b28d04c3bbf865ebe258e31aeb094
lokman524 <79215944+lokman524@users.noreply.github.com>
2025-07-06 18:36:57 +0800

    Yeah

--- No paper/components/Results.tsx
+++ No paper/components/Results.tsx
@@ -4,7 +4,7 @@ import { router } from 'expo-router';
 import { images } from '@/constants/images';
 import { Question } from '@/app/(pages)/quiz/Question/[id]';
 
-const Results = ({userAnswers, questionBank, restartQuiz, startTime} : {userAnswers : string[], questionBank : Question[], restartQuiz: any, startTime: number }) => {
+const Results = ({userAnswers, questionBank, restartQuiz, startTime, finishTime, call, isTimerEnabled, numberOfQuestions} : {userAnswers : string[], questionBank : Question[], restartQuiz: any, startTime: number, finishTime: number, call: string, isTimerEnabled: string, numberOfQuestions: number}) => {
 
     function getScore(){
         let finalScore = 0;
@@ -18,17 +18,22 @@ const Results = ({userAnswers, questionBank, restartQuiz, startTime} : {userAnsw
 
     function handleGoBack (){
         restartQuiz();
-        router.replace("/subject_selection");
+        if (call === "subject_selection")
+            router.replace("/subject_selection");
+        else if (call === "saved")
+            router.dismissTo("/(tabs)/saved")
     }
 
     const score = getScore();
+
+    //console.log(finishTime + "-" + startTime);
 
     return (
         <View className='flex-1 bg-primary'>
             <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
             <Text className='text-white mt-10'>Quiz completed!</Text>
-            <Text className='text-white'>Your Score: {score/questionBank.length * 100}% ({score}/{questionBank.length})</Text>
-            <Text className='text-white mt-5'>Time Used: {((new Date().getTime()) - startTime) / 1000} seconds</Text>
+            <Text className='text-white'>Your Score: {score/numberOfQuestions * 100}% ({score}/{numberOfQuestions})</Text>
+            {isTimerEnabled === "true" && <Text className='text-white mt-5'>Time Used: {(finishTime - startTime) / 1000} seconds</Text>}
             <Button title='go back' onPress={handleGoBack}/>
         </View>
     )
