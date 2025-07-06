7a8673570ecbaa36570b0d853368a5e9365a8817
lokman524 <79215944+lokman524@users.noreply.github.com>
2025-07-06 18:35:07 +0800

    yeah

--- No paper/app/(pages)/subject_selection.tsx
+++ No paper/app/(pages)/subject_selection.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 import { Image, Pressable, ScrollView, Text, View } from 'react-native'
 
 
-export const questionBankAvaliable: {courseName : string, courseID : string, questionCount : number} [] = [
-  { courseName: 'Math', courseID : "1204889572890471", questionCount: 2 },
-  { courseName: 'Science', courseID: "5987259123456789" ,  questionCount: 1 },
+export const questionBankAvailable: {courseName : string, courseID : string, questionCount : number} [] = [
+  { courseName: 'Math', courseID : "1204889572890471", questionCount: 3 },
+  { courseName: 'Science', courseID: "5987259123456789" ,  questionCount: 2 },
 ];
 
 const subject_selection = () => {
@@ -16,7 +16,7 @@ const subject_selection = () => {
       <Text className="text-5xl text-white font-bold mt-5 mb-3">題庫</Text>
       <ScrollView>
         <Text className="text-white" style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20, }}>Select a Subject</Text>
-        {questionBankAvaliable.map((questionBank) => (
+        {questionBankAvailable.map((questionBank) => (
           <Link
             key={questionBank.courseID}
             href={{
